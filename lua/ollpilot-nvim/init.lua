cfdcd52dc7ff1eb4d11013435a0dcea2cd92a2fc
Maksym Bovtun <maksymbo@wix.com>
2025-07-11 15:39:58 +0300

    init upd

--- lua/ollpilot-nvim/init.lua
+++ lua/ollpilot-nvim/init.lua
@@ -1,4 +1,4 @@
-local ollama_path = "ollpilot-nvim.ollpilot"
+local ollpilot_path = "ollpilot-nvim.ollpilot"
 if vim.fn.getcwd() == "/Users/maksymbo/Desktop/www/pets/ollpilot" then
 	package.path = package.path
 		.. ";/Users/maksymbo/Desktop/www/pets/ollpilot/lua/?.lua"
@@ -7,18 +7,17 @@ if vim.fn.getcwd() == "/Users/maksymbo/Desktop/www/pets/ollpilot" then
 		.. ";/Users/maksymbo/Desktop/www/pets/ollpilot/lua/ollpilot-nvim/?/init.lua"
 end
 
-local ollama = require(ollama_path)
+local M = require(ollpilot_path)
 -- Setup with default or user configuration
-ollama.setup()
-local ollama = require(ollama_path)
--- Setup with default or user configuration
-ollama.setup()
+M.setup()
 
 -- Expose commands to the user
 vim.api.nvim_create_user_command("Ollpilot", function()
-	ollama.open_ollpilot_window()
+	M.open_ollpilot_window()
 end, {})
 
 vim.api.nvim_create_user_command("OllpilotSuggest", function()
-	ollama.suggest_line_solution()
+	M.suggest_line_solution()
 end, { desc = "Get suggestion for current line" })
+
+return M
